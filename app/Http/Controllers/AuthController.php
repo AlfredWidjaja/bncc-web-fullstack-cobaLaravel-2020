e008ae163e073c76f78f0eb752d24a7db4f8528d
Alfred Widjaja <alfredwidjaja01@gmail.com>
2021-03-13 19:54:33 +0700

    Webmaster Fullstack Week 12

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function signUp (Request $request){
+        $firstName = $request->input('firstName');
+        $lastName = $request->input('lastName');
+        return view('/welcome', ['firstName'=> $firstName, 'lastName'=>$lastName]);
+    }
+}
---
+++ resources/views/Welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Welcome New Member</h2>
+        <h3>Welcome {{$firstName}} </h3>
+        <p>Nama depan: {{$firstName}}</p>
+        <p>Nama belakang: {{$lastName}}</p>
+        <h4>Have fun join in BNCCBook</h4>
+    </body>
+</html>
